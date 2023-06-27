893f9be9e97dc5e72d5d139cec713234f4bdc66d
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-27 11:57:24 +0100

    account test 5 & 6. Modification to Mock

--- spec/accountTests.spec.js
+++ spec/accountTests.spec.js
@@ -1,17 +1,17 @@
 import Account from '../src/Account.js';
 
 class MockTransaction {
-
+    getTransactionID = () => 'New Transaction1';
 }
 
-describe('Account Tests', () => {
+describe('Account Tests - User Story 1', () => {
 
     let account;
-    let transaction;
+    let transaction1;
 
     beforeEach(() => {
         account = new Account('Test Account');
-        transaction = new MockTransaction('New Transaction');
+        transaction1 = new MockTransaction();
     });
 
     it('test 1 - should create a new instance of an Account', () => {
@@ -39,10 +39,10 @@ describe('Account Tests', () => {
         const addTransactionSpy = spyOn(account, 'addTransaction')
 
         //Act
-        account.addTransaction(transaction);
+        account.addTransaction(transaction1);
 
         //Assert
-        expect(addTransactionSpy).toHaveBeenCalledOnceWith(transaction);
+        expect(addTransactionSpy).toHaveBeenCalledOnceWith(transaction1);
 
     });
 
@@ -55,6 +55,28 @@ describe('Account Tests', () => {
 
         //Assert
         expect(getTransactionsSpy).toHaveBeenCalled;
+    });
+
+    it('test 5 - should return transaction array length of 1 after a transaction is made', () => {
+        //Arrange
+
+        //Act
+        account.addTransaction(transaction1);
+
+        //Assert
+        expect(account.getTransactions().length).toBe(1);
+
+    });
+
+    it('test 6 - should return New Transaction1 as accountID. Checking object added is expected ', () => {
+        //Arrange
+
+        //Act
+        account.addTransaction(transaction1);
+
+        //Assert
+        expect(account.getTransactions()[0].getTransactionID()).toBe('New Transaction1');
+
     });
 
 
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,5 +1,18 @@
 class Transaction {
 
+    #transactionID;
+
+    constructor(transactionID) {
+        this.#transactionID = transactionID;
+    }
+
+    getTransactionID() {
+        return this.#transactionID;
+    }
+
+    setTransactionID(transactionID) {
+        this.#transactionID = transactionID;
+    }
 }
 
 export default Transaction;
