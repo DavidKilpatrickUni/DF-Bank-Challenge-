32f6cf230df54c938c9308dc872e3d5c59227603
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-27 11:35:51 +0100

    Account test 4

--- spec/accountTests.spec.js
+++ spec/accountTests.spec.js
@@ -1,15 +1,20 @@
 import Account from '../src/Account.js';
 
+class MockTransaction {
+
+}
+
 describe('Account Tests', () => {
 
     let account;
+    let transaction;
 
     beforeEach(() => {
         account = new Account('Test Account');
-        transaction = new Transaction('New Transaction');
+        transaction = new MockTransaction('New Transaction');
     });
 
-    it('should create a new instance of an Account', () => {
+    it('test 1 - should create a new instance of an Account', () => {
         //Arrange
 
         //Act
@@ -18,25 +23,38 @@ describe('Account Tests', () => {
         expect(account).toBeInstanceOf(Account);
     });
 
-    it('should call addTransaction method', () => {
+    it('test 2 - should call addTransaction method', () => {
         //Arrange
-
         const addTransactionSpy = spyOn(account, 'addTransaction')
+
         //Act
+        account.addTransaction();
 
         //Assert
         expect(addTransactionSpy).toHaveBeenCalled;
     });
 
-    it('should call addTransaction method with new transaction object', () => {
+    it('test 3 - should call addTransaction method with new transaction object', () => {
         //Arrange
-
         const addTransactionSpy = spyOn(account, 'addTransaction')
+
         //Act
+        account.addTransaction(transaction);
 
         //Assert
         expect(addTransactionSpy).toHaveBeenCalledOnceWith(transaction);
 
+    });
+
+    it('test 4 - should call getTransactions method', () => {
+        //Arrange
+        const getTransactionsSpy = spyOn(account, 'getTransactions')
+
+        //Act
+        account.getTransactions();
+
+        //Assert
+        expect(getTransactionsSpy).toHaveBeenCalled;
     });
 
 
