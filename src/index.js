5e3d289f73397b75c727c0487c66e0729bfb3e14
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-28 10:46:18 +0100

    start work on date

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Account from './Account.js';
 import Printer from './Printer.js';
 import Transaction from './Transaction.js';
 
-const account = new Account('my account');
+const account = new Account();
 
 account.accountDeposit(new Transaction(account.getBalance()), 1000.00)
 account.accountDeposit(new Transaction(account.getBalance()), 2000.00)
