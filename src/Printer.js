12b9fedccc735878312fe1c293b6a89177d2ad17
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-28 10:44:30 +0100

    Printer: added padding to output and IF statement to display blank strings instead of 0 (zero)

--- src/Printer.js
+++ src/Printer.js
@@ -11,11 +11,11 @@ class Printer {
         function displayTransactions(transaction) {
 
             let date = transaction.getDate();
-            let credit = transaction.getAmountDeposited().toFixed(2);
-            let debit = transaction.getAmountWithdrawn().toFixed(2);
+            let credit = (transaction.getAmountDeposited() != 0) ? transaction.getAmountDeposited().toFixed(2) : '';
+            let debit = (transaction.getAmountWithdrawn() != 0) ? transaction.getAmountWithdrawn().toFixed(2) : '';
             let balance = transaction.getTransactionBalance().toFixed(2);
 
-            console.log(`${date} || ${credit}|| ${debit}|| ${balance}`)
+            console.log(`12/12/12 || ${credit.padEnd(8)}|| ${debit.padEnd(7)}|| ${balance}`)
 
         }
 
