1404fe1497a6d142bdb7ac7f3a1adf2684669140
David Kilpatrick <Davidkilpatrick@outlook.com>
2023-06-28 09:37:46 +0100

    Printer test 32

--- spec/printerTest.spec.js
+++ spec/printerTest.spec.js
@@ -22,18 +22,61 @@ class MockTransaction3 {
     getTransactionBalance = () => 2500;
 }
 
+class MockAccount {
+
+};
+
 describe('Printer Tests - User Story 4', () => {
+
+    let account;
+    let transaction1, transaction2, transaction3;
+
+    beforeEach(() => {
+        account = new MockAccount();
+        transaction1 = new MockTransaction1();
+        transaction2 = new MockTransaction2();
+        transaction3 = new MockTransaction3();
+    });
+
+    afterEach(() => {
+        account = undefined;
+        transaction1 = undefined;
+        transaction2 = undefined;
+        transaction3 = undefined;
+    });
+
 
     it('test 30 - should call static Printer method called displayTransactions', () => {
         //Arrange
         const displayTransactionsSpy = spyOn(Printer, 'displayTransactions')
-        const amount = 1000;
 
         //Act
         Printer.displayTransactions();
 
         //Assert
         expect(displayTransactionsSpy).toHaveBeenCalled;
+    });
+
+    it('test 31 - should interact with console.log when displayTransactions called', () => {
+        //Arrange
+        const consoleSpy = spyOn(console, 'log')
+
+        //Act
+        Printer.displayTransactions();
+
+        //Assert
+        expect(consoleSpy).toHaveBeenCalled;
+    });
+
+    it('test 32 - should call static Printer method called displayTransactions with parameter', () => {
+        //Arrange
+        const displayTransactionsSpy = spyOn(Printer, 'displayTransactions')
+
+        //Act
+        Printer.displayTransactions(transaction1);
+
+        //Assert
+        expect(displayTransactionsSpy).toHaveBeenCalledWith(transaction1);
     });
 
 })
